60bc1de559920244377a36ba8dc62a7fba9965a1
FidaHussain <36170696+FidaHussain87@users.noreply.github.com>
2023-11-08 20:04:23 +0100

    core origin updated on backendside for vercel end point

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,9 +4,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'https://weather-app2-0-tau.vercel.app/', // Replace with your frontend URL
+    origin: 'https://weather-app2-0-tau.vercel.app', // Replace with your frontend URL
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type, Accept',
+    credentials: true,
   });
   await app.listen(8000);
 }
